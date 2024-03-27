03185723bddca2b7f9d3e988a11c12edbf3f5562
Morfinix <morfinic@proton.me>
2024-03-27 15:50:04 +0100

    Gongaga

--- Zad1.py
+++ Zad1.py
@@ -1,61 +1,69 @@
-from PIL import Image
-from PIL.TiffTags import TAGS
-
-
-# with Image.open("monke.tiff") as img:
-#     meta_dict = {TAGS[key] : img.tag[key] for key in img.tag_v2}
-#
-# print(meta_dict)
-
-bajty = []
-i = 0
+IFD = list()
 
 with open("monke.tiff", "rb") as f:
-    while(byte := f.read(1)):
-        i += 1
-        bajty.append(byte)
+    header = f.read(8)
 
-header = bajty[:8]
-#wykrywanie w jaki sposob bajty sa zapisywane
-if header[0] == b'I' and header[1] == b'I':
-    endian = "little"
-else:
-    endian = "big"
+    if header[:2] == b'II':
+        endian = "little"
+    else:
+        endian = "big"
 
-sekcje = []
-sekcje.append(header)
+    offset_0 = int.from_bytes(header[4:], endian)
+    i = offset_0
 
-offset_0 = int.from_bytes(header[4] + header[5] + header[6] + header[7], endian)
-i = offset_0
+    while i != 0:
+        f.seek(i)
+        num_entries = int.from_bytes(f.read(2), endian)
+        ifd_size = (num_entries * 12) + 6
+        ifd_data = f.read(ifd_size)
 
-while i < len(bajty):
-    liczba_sekcji_b = b'0'
-    for j in range(i, i + 2):                                    #zczytywanie ile sekcji idf jest w kolejnych bajtach
-        liczba_sekcji_b += bajty[j]
-    liczba_sekcji = int.from_bytes(liczba_sekcji_b, endian)
-    k = 0
-    for j in range(i, i+2+(liczba_sekcji*12)):                   #zczytywanie sekcji idf do nastepnego offsetu
-        sekcja = []
-        for h in range(j + k + 2, j + k + 12 + 2):                       #zczytywanie pojedynczej sekcji
-            sekcja.append(bajty[h])
-        sekcje.append(sekcja)
-        print(h, sekcja)
-        k += 11
-    offset = int.from_bytes(bajty[i+2+(liczba_sekcji*12)] + bajty[i+3+(liczba_sekcji*12)], endian)
-    i += i+2+(liczba_sekcji*12) + offset
+        next_ifd_offset = int.from_bytes(f.read(4), endian)
 
-for i in range(1, 10):
-    print(sekcje[i])
-# sekcja = []
-# for j in bajty:
-#     if i == 18:
-#         sekcje.append(sekcja)
-#         i = 0
-#         sekcja = []
-#     sekcja.append(j)
-#     i += 1
+        IFD.append(ifd_data)
+
+        i = next_ifd_offset
+
+IFD = IFD[:-1]
+
+print("Number of IFD:", len(IFD))
+for elem in IFD:
+    print(elem)
+
+# header = bajty[:8]
 #
-# for i in sekcje:
-#     print(i)
-print(offset_0)
-print(int.from_bytes(sekcje[1][2] + sekcje[1][3], endian))
+# # wykrywanie w jaki sposob bajty sa zapisywane
+# if header[0] == b'I' and header[1] == b'I':
+#     endian = "little"
+# else:
+#     endian = "big"
+#
+# sekcje = list()
+# sekcje.append(header)
+#
+# offset_0 = int.from_bytes(header[4] + header[5] + header[6] + header[7], endian)
+# i = offset_0
+#
+# while i != 0:
+#     liczba_sekcji_b = b'0'
+#
+#     for j in range(i, i + 2):  # zczytywanie ile sekcji idf jest w kolejnych bajtach
+#         liczba_sekcji_b += bajty[j]
+#     liczba_sekcji = int.from_bytes(liczba_sekcji_b, endian)
+#
+#     k = 0
+#     for j in range(i, 6 + (liczba_sekcji * 12)):  # zczytywanie sekcji idf do nastepnego offsetu
+#         sekcja = []
+#         for h in range(j + k + 2, j + k + 12 + 2):  # zczytywanie pojedynczej sekcji
+#             sekcja.append(bajty[h])
+#         sekcje.append(sekcja)
+#         print(h, sekcja)
+#         k += 11
+#
+#     offset = int.from_bytes(bajty[i + 2 + (liczba_sekcji * 12)] + bajty[i + 3 + (liczba_sekcji * 12)], endian)
+#     i += i + 2 + (liczba_sekcji * 12) + offset
+#
+# for i in range(1, 10):
+#     print(sekcje[i])
+#
+# print(offset_0)
+# print(int.from_bytes(sekcje[1][2] + sekcje[1][3], endian))
