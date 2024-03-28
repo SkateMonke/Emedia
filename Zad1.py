d14abbc4b3b2ac88f4bf8fc5d08ca03a383fd88a
Morfinix <morfinic@proton.me>
2024-03-28 16:12:30 +0100

    Podział na Tagi

--- Zad1.py
+++ Zad1.py
@@ -1,4 +1,5 @@
 IFD = list()
+dirEntry = []
 
 with open("monke.tiff", "rb") as f:
     header = f.read(8)
@@ -10,60 +11,39 @@ with open("monke.tiff", "rb") as f:
 
     offset_0 = int.from_bytes(header[4:], endian)
     i = offset_0
+    ifd_size = None
 
     while i != 0:
         f.seek(i)
         num_entries = int.from_bytes(f.read(2), endian)
+
+        if not num_entries:
+            break
+
         ifd_size = (num_entries * 12) + 6
         ifd_data = f.read(ifd_size)
-
         next_ifd_offset = int.from_bytes(f.read(4), endian)
 
         IFD.append(ifd_data)
 
         i = next_ifd_offset
 
-IFD = IFD[:-1]
+        tmpList = list()
+        for j in range(0, ifd_size - 6, 12):
+            tmpList.append(
+                {
+                    "tag": int.from_bytes(ifd_data[j:j + 2], endian),
+                    "type": int.from_bytes(ifd_data[j + 2:j + 4], endian),
+                    "count": int.from_bytes(ifd_data[j + 4:j + 8], endian),
+                    "valueOffset": int.from_bytes(ifd_data[j + 8:j + 12], endian)
+                }
+            )
+        dirEntry.append(tmpList)
 
 print("Number of IFD:", len(IFD))
 for elem in IFD:
     print(elem)
 
-# header = bajty[:8]
-#
-# # wykrywanie w jaki sposob bajty sa zapisywane
-# if header[0] == b'I' and header[1] == b'I':
-#     endian = "little"
-# else:
-#     endian = "big"
-#
-# sekcje = list()
-# sekcje.append(header)
-#
-# offset_0 = int.from_bytes(header[4] + header[5] + header[6] + header[7], endian)
-# i = offset_0
-#
-# while i != 0:
-#     liczba_sekcji_b = b'0'
-#
-#     for j in range(i, i + 2):  # zczytywanie ile sekcji idf jest w kolejnych bajtach
-#         liczba_sekcji_b += bajty[j]
-#     liczba_sekcji = int.from_bytes(liczba_sekcji_b, endian)
-#
-#     k = 0
-#     for j in range(i, 6 + (liczba_sekcji * 12)):  # zczytywanie sekcji idf do nastepnego offsetu
-#         sekcja = []
-#         for h in range(j + k + 2, j + k + 12 + 2):  # zczytywanie pojedynczej sekcji
-#             sekcja.append(bajty[h])
-#         sekcje.append(sekcja)
-#         print(h, sekcja)
-#         k += 11
-#
-#     offset = int.from_bytes(bajty[i + 2 + (liczba_sekcji * 12)] + bajty[i + 3 + (liczba_sekcji * 12)], endian)
-#     i += i + 2 + (liczba_sekcji * 12) + offset
-#
-# for i in range(1, 10):
-#     print(sekcje[i])
-#
-# print(offset_0)
-# print(int.from_bytes(sekcje[1][2] + sekcje[1][3], endian))
+for elem in dirEntry:
+    for xd in elem:
+        print(xd)
