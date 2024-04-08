41edbf3422fbcf30c744c75a0b11617b66a5ed1c
SkateMonke <bartlomiej.gosciniak7@gmail.com>
2024-04-08 15:35:10 +0200

    dodano słownik z tagami baseline oraz skrócono o jednego if-a zczytywanie value/offset

--- Zad1.py
+++ Zad1.py
@@ -19,7 +19,46 @@ type_dict = {
     "12": 8
 }
 
-with open("monke.tiff", "rb") as f:
+baseline_tags = {
+    "NewSubfileType": 254,
+    "SubfileType": 255,
+    "ImageWidth": 256,
+    "ImageLength": 257,
+    "BitsPerSample": 258,
+    "Compression": 259,
+    "PhotometricInterpretation": 262,
+    "Threshholding": 263,
+    "CellWidth": 264,
+    "CellLength": 265,
+    "FillOrder": 266,
+    "ImageDescription": 270,
+    "Make": 271,
+    "Model": 272,
+    "StripOffsets": 273,
+    "Orientation": 274,
+    "SamplesPerPixel": 277,
+    "RowsPerStrip": 278,
+    "StripByteCount": 279,
+    "MinSampleValue": 280,
+    "MaxSampleValue": 281,
+    "XResolution": 282,
+    "YResolution": 283,
+    "PlanarConfiguration": 284,
+    "FreeOffsets": 288,
+    "FreeByteCounts":  289,
+    "GrayResponseUnit": 290,
+    "GrayResponseCurve": 291,
+    "ResolutionUnit": 296,
+    "Software": 305,
+    "DateTime": 306,
+    "Artist": 315,
+    "HostComputer": 316,
+    "ColorMap": 320,
+    "ExtraSamples": 338,
+    "Copyright": 33432
+}
+
+with open("at3_1m4_01.tif", "rb") as f:
     header = f.read(8)
     if header[:2] == b'II':
         endian = "little"
@@ -51,14 +90,12 @@ with open("monke.tiff", "rb") as f:
             count_tmp = int.from_bytes(ifd_data[j + 4:j + 8], endian)
             tmp_val = count_tmp * type_dict[str(type_tmp)]
 
-            if count_tmp == 1:
+            if count_tmp == 1 or tmp_val > 4:
                 valueOffset_tmp = int.from_bytes(ifd_data[j + 8:j + 12], endian)
             elif count_tmp <= 4 and tmp_val <= 4:
                 valueOffset_tmp = (split_list(ifd_data[j + 8:j + 12], 4//count_tmp))
                 for h in range(0, len(valueOffset_tmp)):
                     valueOffset_tmp[h] = int.from_bytes(valueOffset_tmp[h], endian)
-            else:
-                valueOffset_tmp = int.from_bytes(ifd_data[j + 8:j + 12], endian)
 
             tmpList.append(
                 {
